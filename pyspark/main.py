f9d50d907bda9899171f8223f5636f790110e33f
modeste kouassi <modeste.kouassi@workace.fr>
2024-06-10 22:12:48 +0200

    modeste

--- pyspark/main.py
+++ pyspark/main.py
@@ -1,11 +1,16 @@
 from pyspark.sql import SparkSession
 from hdfs import InsecureClient
+from pyspark.sql import Row
+import time
 
-mongo_uri = "mongodb://mongodb:27017/mydb"
+
+#mongo_uri = "mongodb://127.0.0.1:27017"
+mongo_uri = "mongodb+srv://medouse15:6TbzE9CqeQFsM26r@imdb.ixiodlp.mongodb.net/"
+
 output_directory = "/user/root/imdb/split/"
 hdfs_url = "http://hadoop-master:50070"
 hdfs_user = "root"
-hdfs_file_path = "/user/root/imdb/name.basics.tsv"
+hdfs_file_path = "/root/hdfs/purchases.txt"
 
 def split_hdfs_file(hdfs_url, hdfs_user, hdfs_file_path, output_directory, chunk_size):
     client = InsecureClient(hdfs_url, user=hdfs_user)
@@ -38,15 +43,111 @@ def main():
         .appName("IMDbDataPipeline") \
         .config("spark.mongodb.input.uri", mongo_uri) \
         .config("spark.mongodb.output.uri", mongo_uri) \
+        .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
         .getOrCreate()
     
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+
+    print(f'The PySpark {spark.version} version is running...')
+
+        
+
     # Diviser le fichier HDFS en parties
-    split_hdfs_file(hdfs_url, hdfs_user, hdfs_file_path, output_directory, 100000000)
+    #split_hdfs_file(hdfs_url, hdfs_user, hdfs_file_path, output_directory, 100000000)
     
     # Transformer et charger les données
-    transform_and_load_data(spark, output_directory, mongo_uri)
-    
-    spark.stop()
+    # transform_and_load_data(spark, output_directory, mongo_uri)
+
+    # Créer une liste de données
+    data = [
+        Row(id=1, name="Alice", age=23),
+        Row(id=2, name="Bob", age=34),
+        Row(id=3, name="Cathy", age=29),
+        Row(id=4, name="David", age=45),
+        Row(id=5, name="Eva", age=31),
+        Row(id=6, name="Frank", age=38),
+        Row(id=7, name="Grace", age=26),
+        Row(id=8, name="Henry", age=41),
+        Row(id=9, name="Ivy", age=36),
+        Row(id=10, name="Jack", age=28)
+    ]
+
+    # Convertir la liste en DataFrame
+    df = spark.createDataFrame(data)
+
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+
+    time.sleep(3)
+
+
+
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    dataFrame = spark.read.format("mongo").option("database", "imdb").option("collection", "film").load()
+
+    dataFrame.printSchema()
+
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+
+    print("L'heure de verité")
+
+    df.printSchema()
+
+    df.write.format("mongo").mode("append").option("database", "imdb").option("collection","mart_data").save()
+
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("---------OK-------------")
+    print("-----------------------")
+    print("-----------------------")
+    print("-----------------------")
+
+    #spark.stop()
+    print("-----------------------")
+
+    time.sleep(1000)
+
 
 if __name__ == "__main__":
     main()
